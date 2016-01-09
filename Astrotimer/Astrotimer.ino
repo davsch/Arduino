393fd5ab6370b256ec0a65e60c93f463bddabda9
davsch <david.j.schulz@gmail.com>
2016-01-09 01:47:38 +0100

    Added functions for sunrise & sunset

--- Astrotimer/Astrotimer.ino
+++ Astrotimer/Astrotimer.ino
@@ -2,71 +2,140 @@
 #include <Sunrise.h>
 #include <DS3231.h>
 
-
+#define DST 1
 
 // Init the DS3231 using the hardware interface
 DS3231  rtc(SDA, SCL);
-Sunrise sunrise(55.32, 18.06, 0);
+
+// Init the sunrise lib with longitude, latitude and timezone (UTC+1/UTC+2)
+Sunrise sunrise(59.3293235, 18.0685808, DST);
+
+// Init the LCD Shield
+LiquidCrystal lcd(8, 9, 4, 5, 6, 7);  
+
+bool blinkColon = true;
 
 void setup()
 {
-  LiquidCrystal lcd(8, 9, 4, 5, 6, 7);  
-  
+  // Define LCD size and clear screen 
   lcd.begin(16,2);
-  lcd.setCursor(0,0);
-
+  
   lcd.clear();
 
+  // Create custom characters for sunrise & sunset
+  byte charUp[8] = {
+    B00000,
+    B00000,
+    B00100,
+    B01110,
+    B11111,
+    B00000,
+    B11111,
+  };
+
+  byte charDown[8] = {
+    B00000,
+    B00000,
+    B11111,
+    B01110,
+    B00100,
+    B00000,
+    B11111,
+  };
+
+  lcd.createChar(0, charUp);
+  lcd.createChar(1, charDown);
+
   rtc.begin();
-  lcd.setCursor(0,1);
+  // If necessary - set date & time
+  //rtc.setDOW(FRIDAY);
+  //rtc.setTime(20,24,0);
+  //rtc.setDate(9, 1, 2016);
 
-  String date = rtc.getDateStr(1, 2);
-  char *tmp;
+  
 
-  tmp = strtok(&date[0], ".");
-  lcd.print("M:");
-  lcd.print(tmp[0]);
-  lcd.print("D:");
-  lcd.print(tmp[1]);
-
-    sunrise.Astronomical(); //Actual, Civil, Nautical, Astronomical
-
-  byte h,m;
-  int t;
-  // t= minutes past midnight of sunrise (6 am would be 360)
-  t=sunrise.Rise(5,5); // (month,day) - january=1
-  if(t>=0){
-    h=sunrise.Hour();
-    m=sunrise.Minute();
-  }
+  sunrise.Actual(); //Actual, Civil, Nautical, Astronomical
+  
+  //lcd.print("Time:" + dateStr);
+  //lcd.setCursor(1, 0);
+  //lcd.print("Sunrise:" + sunriseTimeStr);
 }
 
 void loop()
 {
-
+  String dateStr = rtc.getDateStr(1, 2);
+  String timeStr = rtc.getTimeStr();
+  String strColon = ":";
+  
+  if(!blinkColon)
+  {
+    strColon = " ";      
+  }
     
+  blinkColon = !blinkColon;
+  
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print(dateStr + " " + timeStr.substring(0, 2) + strColon + timeStr.substring(3, 5));
+  lcd.setCursor(0, 1);  
+  
+  String strSunrise = getSunrise(dateStr);
+
+  lcd.write("*");
+  lcd.write(byte(0));
+  lcd.print(strSunrise);
+
+  String strSunset = getSunset(dateStr);
+  
+  lcd.write(" *");
+  lcd.write(byte(1));
+  lcd.print(strSunset);
+      
+  delay(1000);
 }
 
-string getSunrise()
-{
-  String date = rtc.getDateStr(1, 2);
-  char *tmp;
-  tmp = strtok(&date[0], ".");
 
+
+String getSunrise(String dateStr)
+{
   byte h, m;
   int t;
-  t = sunrise.Rise(tmp[0], tmp[1]);
+
+  int intYear = dateStr.substring(0, 2).toInt();
+  int intMonth = dateStr.substring(3, 5).toInt();
+  int intDay = dateStr.substring(6, 8).toInt();
+  
+  t = sunrise.Rise((char)intMonth, (char)intDay);
+
+  if(t>0){
+    h = sunrise.Hour();
+    m = sunrise.Minute();
+  }
+    
+  return String(h) + ":" + String(m);
+}
+
+String getSunset(String dateStr)
+{
+  byte h, m;
+  int t;
+
+  int intYear = dateStr.substring(0, 2).toInt();
+  int intMonth = dateStr.substring(3, 5).toInt();
+  int intDay = dateStr.substring(6, 8).toInt();
+  
+  t = sunrise.Set((char)intMonth, (char)intDay);
 
   if(t>0){
     h = sunrise.Hour();
     m = sunrise.Minute();
   }
 
-  return 
-}
-
-string getSunset()
-{
-  
+  if(m < 10){
+    return String(h) + ":0" + String(m);
+  }
+  else{
+    return String(h) + ":" + String(m);
+  }
 }
 
