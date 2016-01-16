bad21efde46fd3771364a579ec617859195975e3
davsch <david.j.schulz@gmail.com>
2016-01-16 11:39:39 +0100

    Added better button handling, refresh modes for display & sunrise calculations

--- Astrotimer/Astrotimer.ino
+++ Astrotimer/Astrotimer.ino
@@ -15,10 +15,18 @@
 #define BUTTON_DELAY 500
 #define BUTTON_TIMEOUT 5000
 
+#define DISPLAY_INACTIVE_MODE 60000
+#define DISPLAY_REFRESH_OPERATING 1000
+#define DISPLAY_REFRESH_SETUP 500
+#define SUNRISE_READ_INTERVAL 60000
+
+unsigned long displayLastRefresh;
+unsigned long sunriseLastRead;
+unsigned long buttonLastRead;
 
 int lcd_key = 0;
 int adc_key_in = 0;
-
+int count = 0;
 // Define UTC + Daylight Save Time offset (1h + 0/1h)
 int intTimeZoneAndDstOffset = 1;
 
@@ -30,6 +38,9 @@ LiquidCrystal lcd(8, 9, 4, 5, 6, 7);
 
 // Define display toggle
 bool displayToggle = true;
+bool displayOff = false;
+
+String colonStr = ":";
 
 // Define date/time set indicator
 bool dateTimeSet = false;
@@ -39,8 +50,9 @@ bool setUpMode = false;
 int setUpType = 0; // 0 = year, 1 = day, 2 = month, 3 = hour, 4 = minute
 void setup()
 {
+  Serial.begin(9600);
   // Do we need to reinitialize Sunrise class due to summer-time?
-
+  
   // Get time from RTC
   setSyncProvider(RTC.get);
   
@@ -55,8 +67,12 @@ void setup()
 
   // Define LCD size and clear screen 
   lcd.begin(16,2);
-  
   lcd.clear();
+
+  
+  buttonLastRead = 0;
+  sunriseLastRead = 59900;
+  displayLastRefresh = 0;
 
   // Create some fancy icons for sunrise & sunset
   byte charUp[8] = {
@@ -90,168 +106,113 @@ void setup()
   //rtc.setDate(9, 1, 2016);
 
   sunrise.Actual(); //Actual, Civil, Nautical, Astronomical
-
+  Serial.println("Init done");
 }
 
 void loop()
 {
-  lcd_key = read_LCD_buttons();
-  
-  switch (lcd_key)
+  //Serial.println(String(millis() - btnPressInterval));
+  if((millis() - buttonLastRead) >= BUTTON_DELAY)
   {
-    case btnUP:
-    {
-      if(setUpMode)
-      {
-        switch (setUpType)
-        {
-          case 0:
-          {
-            // Years
-          }
-          case 1:
-          {
-            // Months
-          }
-          case 2:
-          {
-            // Days
-          }
-          case 3:
-          {
-            // Hours
-            adjustTime(3600);
-          }
-          case 4:
-          {
-            // Minutes
-            adjustTime(60);
-          }
-        }
-      }
-    }
-    case btnDOWN:
-    {
-      if(setUpMode)
-      {
-        switch (setUpType)
-        {
-          case 0:
-          {
-            // Years
-          }
-          case 1:
-          {
-            // Months
-          }
-          case 2:
-          {
-            // Days
-          }
-          case 3:
-          {
-            // Hours
-            adjustTime(-3600);
-          }
-          case 4:
-          {
-            // Minutes
-            adjustTime(-60);
-          }
-        }
-      }
-    }
-    case btnLEFT:
-    {
-      if(setUpMode)
-      {
-        if(setUpType == 0)
-        {
-          setUpType = 4;
-        }
-        else
-        {
-          setUpType = setUpType - 1;
-        }
-      }
-    }
-    case btnRIGHT:
-    {
-      if(setUpMode)
-      {
-        if(setUpType == 4)
-        {
-          setUpType = 0;
-        }
-        else
-        {
-          setUpType = setUpType + 1;
-        }
-      }
-    }
-    case btnSELECT:
-    {
-      setUpMode = !setUpMode;
-    }
-  }
-  
-  delay(BUTTON_DELAY);
-  
-  String colonStr = ":";
-  
-  if(!setUpMode && !displayToggle)
-  {
-    colonStr = " ";      
-  }
     
-  displayToggle = !displayToggle;
-  
-  lcd.clear();
-  lcd.setCursor(0, 0);
-
-  if(setUpMode && setUpType < 3 && displayToggle)
-  {    
-    String strYear = String(year());
-    String strMonth = getDoubleDigit(month());
-    String strDay = getDoubleDigit(day());
-
-    if(setUpType == 0)
-      strYear = "    ";
-    else if(setUpType == 1)
-      strMonth = "  ";
-    else if(setUpType == 2)
-      strDay = "  ";
-
-    lcd.print(strYear + ":" + strMonth + ":" + strDay);
+    lcd_key = read_LCD_buttons();
+    
+    if(lcd_key == btnSELECT)
+    {
+      Serial.println("Select pressed");
+    }
+    else if(lcd_key == btnLEFT)
+    {
+      Serial.println("Left pressed");
+    }
+    else if(lcd_key == btnRIGHT)
+    {
+      Serial.println("Right pressed");
+    }
+    else if(lcd_key == btnUP)
+    {
+      Serial.println("Up pressed");
+    }
+    else if(lcd_key == btnDOWN)
+    {
+      Serial.println("Down pressed");
+    }
+    buttonLastRead = millis();
+    
   }
-  else 
-    lcd.print(String(year()) + ":" + getDoubleDigit(month()) + ":" + getDoubleDigit(day()));
- 
+
+
+    
   
-  lcd.setCursor(11, 0);
-  if(setUpMode && setUpType > 2 && displayToggle)
+
+  if((millis() - displayLastRefresh) >= DISPLAY_REFRESH_OPERATING)
   {
-    if(setUpMode != 3)
-      lcd.print(getDoubleDigit(hour()) + ":  ");
+    displayToggle = !displayToggle;
+
+    if(!setUpMode && !displayToggle)
+    {
+      colonStr = " ";      
+    }
     else
-      lcd.print(String("  :" + getDoubleDigit(minute())));
+    {
+      colonStr = ":";
+    }
     
-  }
-  else
-    lcd.print(getDoubleDigit(hour()) + colonStr + getDoubleDigit(minute()));
+    Serial.println("Refreshing LCD");
+    lcd.setCursor(0, 0);
+    lcd.print("                ");
+    lcd.setCursor(0, 0);
+
+    if(setUpMode && setUpType < 3 && displayToggle)
+    {    
+      String strYear = String(year());
+      String strMonth = getDoubleDigit(month());
+      String strDay = getDoubleDigit(day());
   
-  String strSunrise = getSunrise(false);
+      if(setUpType == 0)
+        strYear = "    ";
+      else if(setUpType == 1)
+        strMonth = "  ";
+      else if(setUpType == 2)
+        strDay = "  ";
+  
+      lcd.print(strYear + ":" + strMonth + ":" + strDay);
+    }
+    else 
+      lcd.print(String(year()) + ":" + getDoubleDigit(month()) + ":" + getDoubleDigit(day()));
+   
+    
+    lcd.setCursor(11, 0);
+    if(setUpMode && setUpType > 2 && displayToggle)
+    {
+      if(setUpMode != 3)
+        lcd.print(getDoubleDigit(hour()) + ":  ");
+      else
+        lcd.print(String("  :" + getDoubleDigit(minute())));
+      
+    }
+    else
+      lcd.print(getDoubleDigit(hour()) + colonStr + getDoubleDigit(minute()));
+    
+    if((millis() - sunriseLastRead) >= SUNRISE_READ_INTERVAL)
+    {
+    
+      String strSunrise = getSunrise(false);
+      
+      lcd.setCursor(0, 1);  
+      lcd.write(byte(0));
+      lcd.print(" " + strSunrise);
+    
+      String strSunset = getSunrise(true);
+      lcd.setCursor(9, 1);  
+      lcd.write(byte(1));
+      lcd.print(" " + strSunset);
+    
+      sunriseLastRead = millis();
+    }
 
-  lcd.setCursor(0, 1);  
-  lcd.write(byte(0));
-  lcd.print(" " + strSunrise);
-
-  String strSunset = getSunrise(true);
-  lcd.setCursor(9, 1);  
-  lcd.write(byte(1));
-  lcd.print(" " + strSunset);
-
-  delay(500);
+    displayLastRefresh = millis();
+  }
 }
 
 String getDoubleDigit(int digit)
@@ -265,15 +226,24 @@ String getDoubleDigit(int digit)
 int read_LCD_buttons()
 {
   adc_key_in = analogRead(0);
+  count = count + 1;
+  Serial.println(String(count));
+  Serial.println(String(adc_key_in));
 
-  if(adc_key_in > 1000) return btnNONE;
-  if (adc_key_in < 50)   return btnRIGHT;  
-  if (adc_key_in < 250)  return btnUP; 
-  if (adc_key_in < 400)  return btnDOWN; 
-  if (adc_key_in < 635)  return btnLEFT; 
-  if (adc_key_in < 890)  return btnSELECT;   
-
-  return btnNONE;
+  if(adc_key_in > 1000) 
+    return btnNONE;
+  else if (adc_key_in >= 0 && adc_key_in <= 10)   
+    return btnRIGHT;  
+  else if (adc_key_in >= 90 && adc_key_in <= 110)  
+    return btnUP; 
+  else if (adc_key_in >= 250 && adc_key_in <= 260)  
+    return btnDOWN; 
+  else if (adc_key_in >= 400 && adc_key_in <= 420)  
+    return btnLEFT; 
+  else if (adc_key_in >=  630 && adc_key_in <= 650)  
+    return btnSELECT;   
+  else
+    return btnNONE;
 
 }
 
